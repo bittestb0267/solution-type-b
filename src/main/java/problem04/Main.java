6439a530d71d376c3c930ee3a900c68fc03b2b37
bit <bit@175.209.90.170>
2019-02-21 10:59:21 +0900

    완료

--- src/main/java/problem04/Main.java
+++ src/main/java/problem04/Main.java
@@ -1,11 +1,32 @@
 package problem04;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 	
-	
-	  int arr[] =
-		  {11,12,11,11,12,11,12,11,12,12,11,12,11,12,12,11,12,11,12, 11};
+	public static void main(String[] args) {
+		 
+		int arr[] = {11,12,11,11,12,11,12,11,12,12,11,12,11,12,12,11,12,11,12, 11};
+		Random r = new Random();
+		Scanner scan = new Scanner(System.in);
+		
+		int num = r.nextInt(4)+1;
+		int count = 0;
+		
+		System.out.println("스페이스나 엔터를 눌러주세요");
+		String response = scan.nextLine();
+		if(response.isEmpty()) {
+			if(arr[num]==12) {
+				
+			}
+			
+		}
+		
+		
+		 
+		
+		 
+	}
 	  
-	  for(int i = 0; i < arr.length; i++) { System.out.println(arr[i]); }
-	 
 }
